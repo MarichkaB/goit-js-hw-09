7ccbb50e3aded3ff23958b545077594225c1824f
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-12 19:16:04 +0300

    add 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const formElem = document.querySelector('.form');
+formElem.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  for (let i = 0; i < formElem.amount.value; i += 1) {
+    createPromise(
+      i + 1,
+      Number(formElem.delay.value) + Number(formElem.step.value) * i
+    )
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
