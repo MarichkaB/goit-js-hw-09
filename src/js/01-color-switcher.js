64a159845786c523716c6cce7b6e6738d466b749
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-12 15:50:29 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+stop.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = 0;
+
+start.addEventListener('click', () => {
+  start.disabled = true;
+  stop.disabled = false;
+
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stop.addEventListener('click', () => {
+  start.disabled = false;
+  stop.disabled = true;
+  clearInterval(timerId);
+});
